6132fa052850335fa4b3a166a82b71b158cf22ee
Arhur5445 <144962918+Arhur5445@users.noreply.github.com>
2024-04-05 20:58:15 -0300

    Add files via upload

---
+++ projetologin/src/main/java/faculdade/login/ProjetoWEB.java
@@ -0,0 +1,13 @@
+package faculdade.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWEB {
+
+    public static void main(String[] args) throws Throwable{
+        SpringApplication.run(ProjetoWEB.class, args);
+
+    }
+}
