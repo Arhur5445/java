07ed04bd6d78def7a50647e1c7db9c5b53fccae5
Arhur5445 <144962918+Arhur5445@users.noreply.github.com>
2024-04-19 16:00:45 -0300

    Add files via upload

---
+++ projetologin/src/main/java/faculdade/login/MinhasRotas.java
@@ -0,0 +1,22 @@
+package faculdade.login;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class MinhasRotas implements  WebMvcConfigurer{
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+
+    }
+    @RequestMapping(value = "/casa" , method = RequestMethod.GET)
+    public String carregarHelllo() {return "hello";
+
+    }
+}
